f6bc0a7012e83249f2de8d8df163912390bd0eae
CoyBeard <83329703+CoyBeard@users.noreply.github.com>
2023-09-02 22:30:12 -0400

    PC Program

---
+++ Code/Camera Calibration test/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace Camera_Calibration_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolStripStatusLabel = new System.Windows.Forms.Label();
+            this.BtnRunCalibration = new System.Windows.Forms.Button();
+            this.buttonStartCal = new System.Windows.Forms.Button();
+            this.pictureBoxFoundCorners = new System.Windows.Forms.PictureBox();
+            this.pictureBoxUndistorted = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.checkBoxShowPreFilter = new System.Windows.Forms.CheckBox();
+            this.buttonSaveCalVals = new System.Windows.Forms.Button();
+            this.buttonLoadCalVals = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFoundCorners)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUndistorted)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBoxFoundCorners, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBoxUndistorted, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonLoadCalVals);
+            this.panel1.Controls.Add(this.buttonSaveCalVals);
+            this.panel1.Controls.Add(this.toolStripStatusLabel);
+            this.panel1.Controls.Add(this.BtnRunCalibration);
+            this.panel1.Controls.Add(this.buttonStartCal);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(394, 114);
+            this.panel1.TabIndex = 0;
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.AutoSize = true;
+            this.toolStripStatusLabel.Location = new System.Drawing.Point(270, 18);
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(35, 13);
+            this.toolStripStatusLabel.TabIndex = 2;
+            this.toolStripStatusLabel.Text = "label1";
+            // 
+            // BtnRunCalibration
+            // 
+            this.BtnRunCalibration.Location = new System.Drawing.Point(143, 38);
+            this.BtnRunCalibration.Name = "BtnRunCalibration";
+            this.BtnRunCalibration.Size = new System.Drawing.Size(94, 23);
+            this.BtnRunCalibration.TabIndex = 1;
+            this.BtnRunCalibration.Text = "Run Calibration";
+            this.BtnRunCalibration.UseVisualStyleBackColor = true;
+            this.BtnRunCalibration.Click += new System.EventHandler(this.BtnRunCalibration_Click);
+            // 
+            // buttonStartCal
+            // 
+            this.buttonStartCal.Location = new System.Drawing.Point(143, 9);
+            this.buttonStartCal.Name = "buttonStartCal";
+            this.buttonStartCal.Size = new System.Drawing.Size(94, 23);
+            this.buttonStartCal.TabIndex = 0;
+            this.buttonStartCal.Text = "Start Calibration";
+            this.buttonStartCal.UseVisualStyleBackColor = true;
+            this.buttonStartCal.Click += new System.EventHandler(this.buttonStartCal_Click);
+            // 
+            // pictureBoxFoundCorners
+            // 
+            this.pictureBoxFoundCorners.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxFoundCorners.Location = new System.Drawing.Point(3, 123);
+            this.pictureBoxFoundCorners.Name = "pictureBoxFoundCorners";
+            this.pictureBoxFoundCorners.Size = new System.Drawing.Size(394, 324);
+            this.pictureBoxFoundCorners.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxFoundCorners.TabIndex = 1;
+            this.pictureBoxFoundCorners.TabStop = false;
+            // 
+            // pictureBoxUndistorted
+            // 
+            this.pictureBoxUndistorted.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxUndistorted.Location = new System.Drawing.Point(403, 123);
+            this.pictureBoxUndistorted.Name = "pictureBoxUndistorted";
+            this.pictureBoxUndistorted.Size = new System.Drawing.Size(394, 324);
+            this.pictureBoxUndistorted.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxUndistorted.TabIndex = 2;
+            this.pictureBoxUndistorted.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.checkBoxShowPreFilter);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(403, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(394, 114);
+            this.panel2.TabIndex = 3;
+            // 
+            // checkBoxShowPreFilter
+            // 
+            this.checkBoxShowPreFilter.AutoSize = true;
+            this.checkBoxShowPreFilter.Location = new System.Drawing.Point(157, 94);
+            this.checkBoxShowPreFilter.Name = "checkBoxShowPreFilter";
+            this.checkBoxShowPreFilter.Size = new System.Drawing.Size(97, 17);
+            this.checkBoxShowPreFilter.TabIndex = 0;
+            this.checkBoxShowPreFilter.Text = "Show Pre Filter";
+            this.checkBoxShowPreFilter.UseVisualStyleBackColor = true;
+            this.checkBoxShowPreFilter.CheckStateChanged += new System.EventHandler(this.checkBoxShowPreFilter_CheckStateChanged);
+            // 
+            // buttonSaveCalVals
+            // 
+            this.buttonSaveCalVals.Location = new System.Drawing.Point(143, 67);
+            this.buttonSaveCalVals.Name = "buttonSaveCalVals";
+            this.buttonSaveCalVals.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonSaveCalVals.Size = new System.Drawing.Size(94, 23);
+            this.buttonSaveCalVals.TabIndex = 3;
+            this.buttonSaveCalVals.Text = "Save Calibration";
+            this.buttonSaveCalVals.UseVisualStyleBackColor = true;
+            this.buttonSaveCalVals.Click += new System.EventHandler(this.buttonSaveCalVals_Click);
+            // 
+            // buttonLoadCalVals
+            // 
+            this.buttonLoadCalVals.Location = new System.Drawing.Point(243, 67);
+            this.buttonLoadCalVals.Name = "buttonLoadCalVals";
+            this.buttonLoadCalVals.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonLoadCalVals.Size = new System.Drawing.Size(118, 23);
+            this.buttonLoadCalVals.TabIndex = 4;
+            this.buttonLoadCalVals.Text = "Load Calibration Vals";
+            this.buttonLoadCalVals.UseVisualStyleBackColor = true;
+            this.buttonLoadCalVals.Click += new System.EventHandler(this.buttonLoadCalVals_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFoundCorners)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUndistorted)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonStartCal;
+        private System.Windows.Forms.PictureBox pictureBoxFoundCorners;
+        private System.Windows.Forms.PictureBox pictureBoxUndistorted;
+        private System.Windows.Forms.Button BtnRunCalibration;
+        private System.Windows.Forms.Label toolStripStatusLabel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.CheckBox checkBoxShowPreFilter;
+        private System.Windows.Forms.Button buttonLoadCalVals;
+        private System.Windows.Forms.Button buttonSaveCalVals;
+    }
+}
+
---
+++ Code/Camera Calibration test/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System.IO;
+//using Emgu.CV.CalibrateCamera;
+
+namespace Camera_Calibration_test
+{
+    public partial class Form1 : Form
+    {
+        private string calibrationImagesDirectory = @"..\..\Checkerboard Image Calibration\Images"; // Set your default directory here  
+
+        private List<Image<Bgr, byte>> calibrationImages = new List<Image<Bgr, byte>>();
+        private int displayImageIndex = 0;
+        private Timer imageDisplayTimer = new Timer();
+        private MCvPoint3D32f[][] cornersObjectPoints;
+        private PointF[][] cornersImagePoints;
+        private Mat cameraMatrix = new Mat();
+        private Mat distCoeffs = new Mat();
+        private Size patternSize = new Size(10, 7);
+        private Size imageSize;
+
+        Image<Bgr, byte> FilteredImg;
+
+        public Form1()
+        {
+            InitializeComponent();
+            imageDisplayTimer.Interval = 100; // Display each image for 1 second
+            imageDisplayTimer.Tick += ImageDisplayTimer_Tick;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void ImageDisplayTimer_Tick(object sender, EventArgs e)
+        {
+            displayImageIndex++;
+            UpdateStatusLabel(displayImageIndex.ToString());
+
+            if (displayImageIndex >= calibrationImages.Count)
+            {
+                displayImageIndex = 0;
+                imageDisplayTimer.Stop();
+                UpdateStatusLabel("Calibration Completed");
+            }
+            else
+            {
+                pictureBoxFoundCorners.Image = calibrationImages[displayImageIndex].ToBitmap();
+                FindCornersAndUpdateImage(calibrationImages[displayImageIndex].Convert<Gray, byte>(), displayImageIndex);
+            }
+        }
+
+        private void buttonStartCal_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowser = new FolderBrowserDialog())
+            {
+                folderBrowser.SelectedPath = calibrationImagesDirectory; // Set your default folder path
+
+                if (folderBrowser.ShowDialog() == DialogResult.OK)
+                {
+                    string[] imagePaths = Directory.GetFiles(folderBrowser.SelectedPath, "*.jpg");
+                    foreach (string imagePath in imagePaths)
+                    {
+                        Image<Bgr, byte> image = new Image<Bgr, byte>(imagePath);
+                        calibrationImages.Add(image);
+                    }
+
+                    if (calibrationImages.Count > 0)
+                    {
+                        //pictureBoxFoundCorners.Image = calibrationImages[0].ToBitmap();
+                        //FindCornersAndUpdateImage(calibrationImages[0]);
+
+                        displayImageIndex = 0;
+                        imageDisplayTimer.Start();
+                        UpdateStatusLabel();
+                    }
+                }
+            }
+        }
+
+        private void FindCornersAndUpdateImage(Image<Gray, byte> image, int index)
+        {
+            Image<Bgr, byte> outImg = calibrationImages[index].Clone();
+
+            VectorOfPointF corners = new VectorOfPointF(patternSize.Width * patternSize.Height);
+            CvInvoke.FindChessboardCorners(image, patternSize, corners, CalibCbType.AdaptiveThresh);
+            if (corners != null)
+            {
+                CvInvoke.CornerSubPix(image, corners, new Size(11, 11), new Size(-1, -1), new MCvTermCriteria(30, 0.1));
+                CvInvoke.DrawChessboardCorners(outImg, patternSize, corners, true);
+            }
+
+            pictureBoxFoundCorners.Image = outImg.ToBitmap();
+        }
+
+        static PointF[][] ConvertListTo2DArray(List<VectorOfPointF> listOfVectors)
+        {
+            int rowCount = listOfVectors.Count;
+            PointF[][] resultArray = new PointF[rowCount][];
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                resultArray[i] = listOfVectors[i].ToArray();
+            }
+
+            return resultArray;
+        }
+
+        private MCvPoint3D32f[] CreateObjectPoints()
+        {
+            List<MCvPoint3D32f> objectPoints = new List<MCvPoint3D32f>();
+            for (int i = 0; i < patternSize.Height; i++)
+            {
+                for (int j = 0; j < patternSize.Width; j++)
+                {
+                    objectPoints.Add(new MCvPoint3D32f(j, i, 0));
+                }
+            }
+            return objectPoints.ToArray();
+        } 
+
+        private void UpdateStatusLabel(string message = "")
+        {
+            toolStripStatusLabel.Text = message;
+        }
+
+        private void checkBoxShowPreFilter_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (checkBoxShowPreFilter.Checked)
+            {
+                pictureBoxUndistorted.Image = calibrationImages[0].ToBitmap();
+            }
+            if (!checkBoxShowPreFilter.Checked)
+            {
+                pictureBoxUndistorted.Image = FilteredImg.ToBitmap();
+            }
+        }
+
+        private void buttonLoadCalVals_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                using (FileStorage fs = new FileStorage(filePath, FileStorage.Mode.Read))
+                {
+                    fs.GetNode("cameraMatrix").ReadMat(cameraMatrix); //fs.Read("cameraMatrix", out cameraMatrix);
+                    fs.GetNode("distCoeffs").ReadMat(distCoeffs);
+                }
+
+                MessageBox.Show("Calibration parameters loaded from file.");
+            }
+
+            Console.WriteLine(cameraMatrix);
+            Console.WriteLine(distCoeffs);
+
+        }
+
+        private void BtnRunCalibration_Click(object sender, EventArgs e)
+        {
+            calibrationImages.Clear();
+            using (FolderBrowserDialog folderBrowser = new FolderBrowserDialog())
+            {
+                folderBrowser.SelectedPath = calibrationImagesDirectory; // Set your default folder path
+
+                if (folderBrowser.ShowDialog() == DialogResult.OK)
+                {
+                    string[] imagePaths = Directory.GetFiles(folderBrowser.SelectedPath, "*.jpg");
+                    foreach (string imagePath in imagePaths)
+                    {
+                        Image<Bgr, byte> image = new Image<Bgr, byte>(imagePath);
+                        calibrationImages.Add(image);
+                    }
+                }
+            }
+            // Ensure you've captured enough images for calibration
+            if (calibrationImages.Count < 10)
+            {
+                UpdateStatusLabel("At least 10 images are required for calibration.");
+                return;
+            }
+
+            imageSize = calibrationImages[0].Size;
+            List<MCvPoint3D32f[]> objectPointsList = new List<MCvPoint3D32f[]>();
+            List<VectorOfPointF> imagePointsList = new List<VectorOfPointF>();
+
+            for (int i = 0; i < calibrationImages.Count; i++)
+            {
+                VectorOfPointF corners = new VectorOfPointF(patternSize.Width * patternSize.Height);
+
+                CvInvoke.FindChessboardCorners(calibrationImages[i].Convert<Gray, byte>(), patternSize, corners, CalibCbType.AdaptiveThresh);
+
+                if (corners == null)
+                {
+                    UpdateStatusLabel("Failed to find corners in one of the images.");
+                    return;
+                }
+
+                CvInvoke.CornerSubPix(calibrationImages[i].Convert<Gray, byte>(), corners, new Size(11, 11), new Size(-1, -1), new MCvTermCriteria(30, 0.1));
+                objectPointsList.Add(CreateObjectPoints());
+                imagePointsList.Add(corners);
+            }
+
+            cornersObjectPoints = objectPointsList.ToArray();
+            cornersImagePoints = ConvertListTo2DArray(imagePointsList);
+
+            Mat[] rvecs, tvecs;
+            //Mat cameraMatrix = new Mat();
+            //Mat distCoeffs = new Mat();
+
+            CvInvoke.CalibrateCamera(
+                cornersObjectPoints,
+                cornersImagePoints,
+                imageSize,
+                cameraMatrix,
+                distCoeffs,
+                CalibType.RationalModel,
+                new MCvTermCriteria(30, 0.1),
+                out rvecs,
+                out tvecs
+            );
+
+            Image<Bgr, byte> inpImg = calibrationImages[0].Clone();
+            CvInvoke.Undistort(calibrationImages[0], inpImg, cameraMatrix, distCoeffs);
+
+            FilteredImg = inpImg;
+            pictureBoxUndistorted.Image = FilteredImg.ToBitmap();
+
+            /*Save Camera Calibration Stats 
+            string outputFileName = "calibration_results.xml";
+            CameraCalibration.StoreCameraParams(outputFileName, imageSize, cameraMatrix, distCoeffs, rvecs, tvecs);
+
+            UpdateStatusLabel("Calibration results saved to " + outputFileName);*/
+        }
+
+        private void buttonSaveCalVals_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+
+                using (FileStorage fs = new FileStorage(filePath, FileStorage.Mode.Write))
+                {
+                    fs.Write(cameraMatrix, "cameraMatrix");
+                    fs.Write(distCoeffs, "distCoeffs");
+                }
+
+                MessageBox.Show("Calibration parameters saved to file.");
+            }
+            Console.WriteLine(cameraMatrix);
+            Console.WriteLine(distCoeffs);
+        }
+    }
+}
